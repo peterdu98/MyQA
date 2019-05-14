3e428623441ca5ef79940d345ed629853811df98
peterdu98 <peterhung998@gmail.com>
2019-05-14 14:03:36 +1000

    Add Answer class and its unit test

---
+++ MyQA/MyQADLL/src/Answer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MyQADLL.src
+{
+    public class Answer
+    {
+        //Fields
+        private string _answer;
+        private int _fetchID;
+        static readonly string textFile = "/Users/peterdu/Projects/C#/QACustomProgram/MyQA/MyQADLL/Resources/correctAnswer.txt";
+        
+        //Constructor
+        public Answer(int id)
+        {
+            _fetchID = id;
+            LoadAnswer();
+        }
+
+        //Properties
+        protected string Answ { get => _answer; }
+        protected int FetchID { get => _fetchID; set => _fetchID = value; }
+
+        //Methods
+        private void LoadAnswer()
+        {
+            //Load the file and then update _answer based on FetchID
+            if (File.Exists(textFile))
+            {
+                string[] lines = File.ReadAllLines(textFile);
+
+                foreach (string line in lines)
+                {
+                    string[] l = line.Split(new char[] { ',' });
+                    if (Convert.ToInt32(l[0]) == FetchID)
+                    {
+                        _answer = l[1];
+                    }
+                }
+            }
+        }
+
+        public Boolean Check(string choice)
+        {
+            bool result = false;
+            LoadAnswer();
+            if (choice == Answ)
+                result = true;
+            return result;
+        }
+    }
+}
---
+++ MyQA/MyQADLL/test/AnswerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using MyQADLL.src;
+
+namespace MyQADLL.test
+{
+    [TestFixture()]
+    public class AnswerTest
+    {
+
+        [Test()]
+        public void CheckTrueAnswerTest()
+        {
+            //The correct answer for Question 1
+            Answer _answer = new Answer(1);
+            string textAnswer = "TAFE Building";
+            bool actualResult = _answer.Check(textAnswer);
+            Assert.IsTrue(actualResult);
+        }
+
+        [Test()]
+        public void CheckFalseAnswerTest()
+        {
+            //The correct answer for Question 2, but the choice is uncorrect
+            Answer _answer = new Answer(2);
+            string textAnswer = "TAFE Building";
+            bool actualResult = _answer.Check(textAnswer);
+            Assert.IsFalse(actualResult);
+        }
+    }
+}
