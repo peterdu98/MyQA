c082cc07d4cf0f07c4afa51e760a940ce9c65e29
peterdu98 <peterhung998@gmail.com>
2019-05-15 15:22:10 +1000

    Add Counter class and its unit test

---
+++ MyQA/MyQADLL/src/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace MyQADLL.src
+{
+    public class Counter
+    {
+        //Fields
+        private int _value;
+
+        //Constructor
+        public Counter()
+        {
+            _value = 0;
+        }
+
+        //Property
+        public int Value { get => _value; }
+
+        //Methods
+        public void Increment() => _value++;
+        public void Reset() => _value = 0;
+        public void Decrease()
+        {
+            if (Value > 0)
+                _value--;
+        }
+    }
+}
---
+++ MyQA/MyQADLL/test/CounterTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using MyQADLL.src;
+
+namespace MyQADLL.test
+{
+    [TestFixture()]
+    public class CounterTest
+    {
+        [Test()]
+        public void CounterInitTest()
+        {
+            Counter counter = new Counter();
+            Assert.AreEqual(0, counter.Value);
+        }
+
+        [Test()]
+        public void IncrementTest()
+        {
+            Counter counter = new Counter();
+            counter.Increment();
+            Assert.AreEqual(1, counter.Value);
+        }
+
+        [Test()]
+        public void ResetTest()
+        {
+            Counter counter = new Counter();
+            counter.Increment();
+            counter.Reset();
+            Assert.AreEqual(0, counter.Value);
+        }
+
+        [Test()]
+        public void PositiveDecreaseTest()
+        {
+            Counter counter = new Counter();
+            counter.Increment();
+            counter.Increment();
+            counter.Decrease();
+            Assert.AreEqual(1, counter.Value);
+        }
+
+        [Test()]
+        public void NegativeDecreaseTest()
+        {
+            Counter counter = new Counter();
+            counter.Decrease();
+            Assert.AreEqual(0, counter.Value);
+        }
+    }
+}
