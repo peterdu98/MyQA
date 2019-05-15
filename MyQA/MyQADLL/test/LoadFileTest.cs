01bb813ad42d3db2abf46633573d16fe1816b719
peterdu98 <peterhung998@gmail.com>
2019-05-15 11:47:42 +1000

    Add Choice class and its unit test

---
+++ MyQA/MyQADLL/test/LoadFileTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace MyQADLL.test
+{
+    [TestFixture()]
+    public class LoadFileTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
