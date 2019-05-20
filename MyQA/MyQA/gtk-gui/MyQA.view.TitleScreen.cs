3dfd9edffdc6b5a1d5354026a677cba7b57d3059
peterdu98 <peterhung998@gmail.com>
2019-05-20 23:48:07 +1000

    Update Resources for GUI

---
+++ MyQA/MyQA/gtk-gui/MyQA.view.TitleScreen.cs
@@ -0,0 +1,10 @@
+
+namespace MyQA.view
+{
+	public partial class TitleScreen
+	{
+		private void Build()
+		{
+		}
+	}
+}
---
+++ MyQA/MyQA/view/TitleScreen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyQA.view
+{
+    public partial class TitleScreen : Gtk.Window
+    {
+        public TitleScreen() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
