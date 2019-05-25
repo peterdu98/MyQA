a447284f0e562b5097b8e7b79d0725c79bf53ed0
peterdu98 <peterhung998@gmail.com>
2019-05-25 12:38:29 +1000

    Added HistoryScreen + Adjusted TitleScreen and MainWindow

---
+++ MyQA/MyQA/gtk-gui/MyQA.view.HistoryScreen.cs
@@ -0,0 +1,10 @@
+
+namespace MyQA.view
+{
+	public partial class HistoryScreen
+	{
+		private void Build()
+		{
+		}
+	}
+}
---
+++ MyQA/MyQA/view/HistoryScreen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyQA.view
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class HistoryScreen : Gtk.Bin
+    {
+        public HistoryScreen()
+        {
+            this.Build();
+        }
+    }
+}
